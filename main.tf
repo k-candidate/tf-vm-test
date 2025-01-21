b28cb888373652ff84cbffbaeddca8ec4b82698f
k-candidate <k-candidate@outlook.com>
2025-01-21 11:22:24 +0100

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.10.0"
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "= 0.7.1"
+    }
+  }
+}
+
 provider "libvirt" {
   uri = var.libvirt_uri
 }
