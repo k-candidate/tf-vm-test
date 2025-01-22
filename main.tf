9e970ebb0941b00739e538d554a5927747d355cc
k-candidate <k-candidate@outlook.com>
2025-01-23 00:33:33 +0100

    testing

--- main.tf
+++ main.tf
@@ -15,14 +15,15 @@ provider "libvirt" {
 module "tf-module-kvm-vm" {
   source                  = "github.com/k-candidate/tf-module-kvm-vm.git?ref=v1.4.1"
   image_source            = "https://cloud-images.ubuntu.com/releases/noble/release/ubuntu-24.04-server-cloudimg-amd64.img"
+  volume_size             = 5368709120
   network_name            = "default"
   vm_name                 = "tf-vm-test"
   use_cloud_init          = true
   cloud_init_cfg_filename = "user-data.cfg"
   cloud_init_vars = {
-    vm_username = "ubuntu"
+    vm_username    = "ubuntu"
     ssh_public_key = var.ssh_public_key
   }
   ssh_private_key = var.ssh_private_key
-  vm_username = "ubuntu"
+  vm_username     = "ubuntu"
 }
